e938df1d66eb379e0ae3f7af99ddac304fd87e7d
Rafal Rozak <rafal.rozak@delivered.io>
2015-12-26 23:01:32 +0100

    add solution for advent of code problem 3.2

--- aoc3/aoc3.go
+++ aoc3/aoc3.go
@@ -16,28 +16,38 @@ func main() {
 	data, err := ioutil.ReadFile(os.Args[1])
 	check(err)
 
-	x, y := 0, 0
+	x1, y1 := 0, 0
+	x2, y2 := 0, 0
+	x, y := &x1, &y1
+	santa := true
 	counter := 0
 
 	type boolmap map[int]bool
 	visited := make(map[int]boolmap)
 
 	for i := range data {
+		if santa {
+			x, y = &x1, &y1
+			santa = false
+		} else {
+			x, y = &x2, &y2
+			santa = true
+		}
 		switch {
 		case data[i] == '^':
-			y += 1
+			*y += 1
 		case data[i] == 'v':
-			y -= 1
+			*y -= 1
 		case data[i] == '>':
-			x += 1
+			*x += 1
 		case data[i] == '<':
-			x -= 1
+			*x -= 1
 		}
-		if visited[x] == nil {
-			visited[x] = make(boolmap)
+		if visited[*x] == nil {
+			visited[*x] = make(boolmap)
 		}
-		if !visited[x][y] {
-			visited[x][y] = true
+		if !visited[*x][*y] {
+			visited[*x][*y] = true
 			counter += 1
 		}
 	}
